786ff85ca7e974845788b7948338a42b51f96fe7
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-22 12:44:10 +0000

    add ; to end of line

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,11 +1,11 @@
 describe('using calculator', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/calculator.html');
-  })
+  });
 
   it('should load the calculator', () => {
     cy.get('[data-cy="calculator"]').should('exist');
-  })
+  });
 
   // Check the buttons are all correctly labeled
   it('checks numeric buttons should be the correct numbers', () => {
@@ -19,26 +19,26 @@ describe('using calculator', () => {
     cy.get('[data-cy="8"]').should('have.text', '8');
     cy.get('[data-cy="9"]').should('have.text', '9');
     cy.get('[data-cy="0"]').should('have.text', '0');
-  })
+  });
 
   it('checks the operator buttons should be the correct operators', () => {
     cy.get('[data-cy="add"]').should('have.text', '+');
     cy.get('[data-cy="minus"]').should('have.text', '-');
     cy.get('[data-cy="multiply"]').should('have.text', '*');
     cy.get('[data-cy="divide"]').should('have.text', '/');
-  })
+  });
 
   it('checks the decimal button is a decimal point', () => {
     cy.get('[data-cy="decimal"]').should('have.text', '.');
-  })
+  });
 
   it('checks the equals button is an equals sign', () => {
     cy.get('[data-cy="equals"]').should('have.text', '=');
-  })
+  });
 
   it('checks the clear button is a clear button', () => {
     cy.get('[data-cy="clear"]').should('have.text', 'C');
-  }) 
+  });
 
   // Check some basic calculations
   it('should perform basic addition of two single digit numbers', () => {
@@ -47,7 +47,7 @@ describe('using calculator', () => {
     cy.get('[data-cy="2"]').click();
     cy.get('[data-cy="equals"]').click();
     cy.get('[data-cy="answer"]').should('have.text', '3');
-  })
+  });
 
   it('should perform basic subtraction of two single digit numbers', () => {
     cy.get('[data-cy="9"]').click();
@@ -55,7 +55,7 @@ describe('using calculator', () => {
     cy.get('[data-cy="4"]').click();
     cy.get('[data-cy="equals"]').click();
     cy.get('[data-cy="answer"]').should('have.text', '5');
-  })
+  });
 
   it('should perform basic multiplication of two single digit numbers', () => {
     cy.get('[data-cy="3"]').click();
@@ -63,7 +63,7 @@ describe('using calculator', () => {
     cy.get('[data-cy="5"]').click();
     cy.get('[data-cy="equals"]').click();
     cy.get('[data-cy="answer"]').should('have.text', '15');
-  })
+  });
 
   it('should perform basic division of two single digit numbers', () => {
     cy.get('[data-cy="8"]').click();
